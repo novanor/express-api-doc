46dfb2cb3a9ea97bdebeb4c2c13a7db20d73752c
Marin Aldimirov <marin.aldimirov@novanor.org>
2018-01-15 13:26:42 +0200

    Cleanup index.js and remove dependencies to libraries

--- index.js
+++ index.js
@@ -1,77 +1,66 @@
 'use strict';
-const _ = require('lodash');
-const csp = require('js-csp');
 const fs = require('fs');
-const path = require('path');
-
-const templateSourcePath = `${__dirname}/template.html`;
-
-const go = csp.go;
-const take = csp.take;
-const chan = csp.chan;
-const putAsync = csp.putAsync;
 
 class Docks {
     constructor(app) {
         this.app = app;
     }
 
-    static clearFile(pathToFile) {
-        const clearStream = fs.createWriteStream(pathToFile, {flags: 'w'});
-        clearStream.write('');
-        clearStream.end();
+    getExamples(examplesPath) {
+        var examples = fs.readFileSync(examplesPath).toString();
+
+        return examples.length ? JSON.parse(examples) : [];
     }
 
-    track(config = {}) {
-        this.writeChannel = chan();
-        const examplesPath = config.paths.apidoc;
-        const self = this;
+    addExampleToFile(examplesPath, example) {
+        return new Promise((resolve, reject) => {
+            try {
+                let examples = this.getExamples(examplesPath);
+                examples.push(example);
 
-        this.writeToFile(examplesPath);
+                fs.writeFileSync(examplesPath, JSON.stringify(examples, null, 2));
+
+                resolve('Example added');
+            } catch (err) {
+                reject(err);
+            }
+        });
+    }
+
+    track(config) {
+        const _this = this;
 
         this.app.use('/*', (req, res, next) => {
             const originalSend = res.send;
             res.send = function(body, ...args) {
-                !self.writeChannel.closed && putAsync(self.writeChannel, {
+                // TODO: remember to remove console.log
+                console.log(body);
+                let example = {
                     url: req.originalUrl,
                     method: req.method,
                     request: req.body,
                     status: res.statusCode,
                     type: res.get('Content-Type'),
-                    response: JSON.parse(body)
-                });
+                    response: JSON.parse(body),
+                };
+
+                _this.addExampleToFile(config.path, example)
+                    .then(success => console.log(success))
+                    .catch(err => console.log(err));
+
                 originalSend.call(this, body, ...args);
             };
             next();
         });
     }
 
-    writeToFile(examplesPath) {
-        const self = this;
-        go(function* () {
-            while (!self.writeChannel.closed) {
-                const data = yield take(self.writeChannel);
-                yield new Promise((res, rej) => {
-                    fs.appendFile(examplesPath, `${JSON.stringify(data, null, 2)},`, (err) => {
-                        if (err) {
-                            return rej(err);
-                        }
-                        return res();
-                    });
-                }).catch((err) => {
-                    throw err;
-                });
-            }
-        });
-    }
-
     getNestedRoutes(middleware, stack) {
         const routes = [];
 
-        _.each(stack, (handler) => {
+        stack.forEach(handler => {
             if (handler.handle.stack) {
                 const nested = this.getNestedRoutes(handler, handler.handle.stack);
-                _.each(nested, (route) => {
+                nested.forEach(route => {
                     routes.push(route);
                 })
             }
@@ -90,7 +79,7 @@ class Docks {
 
     getAllRoutes() {
         const routes = [];
-        _.each(this.app._router.stack, (middleware) => {
+        this.app._router.stack.forEach(middleware => {
             if (middleware.route) {
                 routes.push({
                     path: middleware.route.path,
@@ -101,7 +90,7 @@ class Docks {
             } else if (middleware.name === 'router') {
 
                 var nested = this.getNestedRoutes(middleware, middleware.handle.stack);
-                _.each(nested, (route) => {
+                nested.forEach(route => {
                     routes.push(route);
                 });
             }
@@ -110,53 +99,61 @@ class Docks {
         return routes;
     }
 
-    static getExamples(examplesPath) {
-        let examples = fs.readFileSync(examplesPath);
-        examples = examples.toString();
-        examples = examples.substring(0, examples.length - 1);
-        examples = `[${examples}]`;
-        return JSON.parse(examples);
+    generate(config) {
+        const routes = this.getAllRoutes();
+        const examples = this.getExamples(config.paths.examples);
+        var examplesByRoute = [];
+
+        routes.forEach((route, index) => {
+            route.id = (index+1).toString();
+            route.params = this.extractParamsFromPath(route.path);
+        });
+
+        examplesByRoute = this.matchExamplesToRoutes(examples, routes);
+
+        // Place all definitions in the template and write to apidoc file
+        fs.writeFileSync(config.paths.apidoc, fs.readFileSync(config.paths.template)
+            .toString()
+            .replace("'{{ROUTES}}'", JSON.stringify(routes, null, 2))
+            .replace("'{{EXAMPLES}}'", JSON.stringify(examplesByRoute, null, 2))
+            .replace("'{{TITLE}}'", JSON.stringify(config.meta.title, null, 2))
+            .replace("'{{DESCRIPTION}}'", JSON.stringify(config.meta.title, null, 2))
+            .replace("'{{CONFIG}}'", JSON.stringify(config, null, 2))
+        );
+
+        // eslint-disable-line no-process-exit
+        process.exit(0);
     }
 
-    generate(config = {}) {
-        const examplesSource = config.paths.examples
-            ? Docks.getExamples(config.paths.examples)
-            : '';
-        const routes = this.getAllRoutes();
-        const examples = [];
-        _.each(routes, (route) => {
-            route.id = _.uniqueId();
-            route.params = _.map((route.path.match(/\/:([^/]+)/ig)), (routePath) => routePath.replace('/:', ''));
+    matchExamplesToRoutes(examples, routes) {
+        var examplesByRoute = [];
+
+        routes.forEach((route, index) => {
             const routeRegexpString = `${route.path.replace(/\//ig, '\\/').replace(/\/:([^/]+)/ig, '/[^\\/]+')}(?:\\?.*)?$`;
-            const routeRegexp = new RegExp(routeRegexpString);
-            route.regexp = routeRegexp.toString();
-            const prefixRegexp = new RegExp(route.prefixRegexp);
-            const examplesForRoute = _.filter(examplesSource, (example) => {
-                const currentRouteRegexp = new RegExp(prefixRegexp.source + routeRegexp.source);
-                return currentRouteRegexp.test(example.url) && route.methods[_.lowerCase(example.method)];
+
+            const examplesForRoute = examples.filter((example) => {
+                return example.url.match(routeRegexpString) && route.methods[example.method.toLowerCase()];
             });
-            route.examplesPresent = !_.isEmpty(examplesForRoute);
-            _.each(examplesForRoute, (e) => {
-                e.id = _.uniqueId();
+
+            route.examplesPresent = !!examplesForRoute.length;
+
+            examplesForRoute.forEach((e, index) => {
+                e.id = (index+1).toString();
             });
-            examples.push({routeId: route.id, rows: examplesForRoute});
-            route.prefixRegexp = prefixRegexp.toString();
+
+            examplesByRoute.push({
+                routeId: route.id,
+                rows: examplesForRoute
+            });
         });
-        let template = config.paths.template
-            ? fs.readFileSync(path.resolve(config.paths.template))
-            : fs.readFileSync(templateSourcePath);
-        template = template.toString();
-        template = template.replace("'{{ROUTES}}'", JSON.stringify(routes, null, 2));
-        template = template.replace("'{{EXAMPLES}}'", JSON.stringify(examples, null, 2));
-        template = template.replace("'{{TITLE}}'", JSON.stringify(config.meta.title, null, 2));
-        template = template.replace("'{{DESCRIPTION}}'", JSON.stringify(config.meta.title, null, 2));
-        template = template.replace("'{{CONFIG}}'", JSON.stringify(config, null, 2));
-        fs.writeFileSync(path.resolve(config.paths.apidoc || 'public/template.html'), template);
-        if (examplesSource) {
-            const examplesPath = path.resolve(config.paths.examples) || path.resolve('public/examples.txt');
-            Docks.clearFile(examplesPath);
-        }
-        process.exit(0); // eslint-disable-line no-process-exit
+
+        return examplesByRoute;
+    }
+
+    extractParamsFromPath(path) {
+        const params = path.match(/\/:([^/]+)/g);
+
+        return params ? params.map(param => param.replace('/:', '')) : [];
     }
 }
 
