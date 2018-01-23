a9a1e250d7e831a8bd7edd8d160fb072dfcd2d08
Marin Aldimirov <m.aldimirov@gmail.com>
2018-01-23 12:54:04 +0200

    Add skip paths to config and error try/catching

--- src/trackExamples.js
+++ src/trackExamples.js
@@ -7,29 +7,38 @@ module.exports = function trackExamples(app, config) {
     app.use('/*', (req, res, next) => {
         const originalSend = res.send;
         res.send = function (body, ...args) {
-            const responseHeaders = {};
-            res.getHeaderNames().forEach(header => {
-                responseHeaders[header] = res.get(header);
-            });
+            try {
+                const responseHeaders = {};
 
-            const example = {
-                url: req.originalUrl,
-                method: req.method,
-                request: {
-                    headers: req.headers,
-                    body: req.body,
-                },
-                status: res.statusCode,
-                type: res.get('Content-Type'),
-                response: {
-                    headers: responseHeaders,
-                    body: JSON.parse(body),
-                },
-            };
+                if (config.skip && config.skip.indexOf(req.originalUrl) > 0) {
+                    throw 'Skipping path';
+                }
 
-            addExampleToFile(config.path, example)
-                .then(success => console.log(success)) // eslint-disable-line no-console
-                .catch(err => console.log(err)); // eslint-disable-line no-console
+                res.getHeaderNames().forEach(header => {
+                    responseHeaders[header] = res.get(header);
+                });
+
+                const example = {
+                    url: req.originalUrl,
+                    method: req.method,
+                    request: {
+                        headers: req.headers,
+                        body: req.body,
+                    },
+                    status: res.statusCode,
+                    type: res.get('Content-Type'),
+                    response: {
+                        headers: responseHeaders,
+                        body: JSON.parse(body),
+                    },
+                };
+
+                addExampleToFile(config.path, example)
+                    .then(success => console.info('\x1b[32m%s\x1b[39m', success))
+                    .catch(err => { throw err; });
+            } catch (err) {
+                console.info('\x1b[31m%s\x1b[39m%s', 'Example not recorded: ', err);
+            }
 
             originalSend.call(this, body, ...args);
         };
