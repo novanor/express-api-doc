ca75113f3553d4875248cbd93c302a4b850632af
Marin Aldimirov <m.aldimirov@gmail.com>
2018-01-25 11:37:56 +0200

    Add models index export :art:

---
+++ src/models/index.js
@@ -0,0 +1,13 @@
+const Collection = require('./collection');
+const Folder = require('./folder');
+const Route = require('./route');
+const Request = require('./request');
+const Example = require('./example');
+
+module.exports = {
+    Collection,
+    Folder,
+    Route,
+    Request,
+    Example,
+};
